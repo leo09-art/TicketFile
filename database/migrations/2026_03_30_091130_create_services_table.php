1f250f6c853d7a7930d4e84e09508b9f1d2b7c8d
georgegale228 <codevort@gmail.com>
2026-04-08 15:46:20 +0000

    ajout des attrinuts des migrations tickets et services

--- database/migrations/2026_03_30_091130_create_services_table.php
+++ database/migrations/2026_03_30_091130_create_services_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('services');
--- database/migrations/2026_03_30_091238_create_tickets_table.php
+++ database/migrations/2026_03_30_091238_create_tickets_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->integer('ticket_number');
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('counter_id')->nullable()->constrained()->onDelete('set null');
+            $table->enum('status', ['en_attente', 'appele', 'traite', 'absent', 'annule'])->default('en_attente');
+            $table->timestamp('called_at')->nullable();
+            $table->timestamp('treated_at')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tickets');
